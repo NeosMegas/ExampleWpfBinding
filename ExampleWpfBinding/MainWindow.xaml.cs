7e961a3a1d511369d0becebb8ef27f3e1217a9d5
Alexander Valiev <64912185+NeosMegas@users.noreply.github.com>
2024-04-18 16:34:43 +0500

    Add project files.

---
+++ ExampleWpfBinding/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExampleWpfBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public List<ExampleItem> ExampleListOfItems { get; set; } = new List<ExampleItem>()
+        {
+            new ExampleItem() { Text = "Вася" },
+            new ExampleItem() { Text = "Петя" }
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ExampleListOfItems.Add(new ExampleItem() { Text = txtAdd.Text });
+            CollectionViewSource.GetDefaultView(lv.ItemsSource).Refresh();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ExampleItem? itemToRemove = cb.SelectedItem as ExampleItem;
+            if (itemToRemove != null) {
+                ExampleListOfItems.Remove(itemToRemove);
+                CollectionViewSource.GetDefaultView(lv.ItemsSource).Refresh();
+                cb.SelectedItem = ExampleListOfItems.FirstOrDefault();
+            }
+        }
+    }
+
+    public class ExampleItem
+    {
+        public string Text { get; set; } = string.Empty;
+    }
+}
